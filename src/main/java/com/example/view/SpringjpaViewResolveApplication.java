fb4a5e9b8f79202d30af14b763fb7d7f919fcf9c
Prashant Chaudhary <prashantchaudharyy1@gmail.com>
2026-01-21 16:40:58 +0530

    Initial commit

---
+++ src/main/java/com/example/view/SpringjpaViewResolveApplication.java
@@ -0,0 +1,13 @@
+package com.example.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaViewResolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpaViewResolveApplication.class, args);
+	}
+
+}
